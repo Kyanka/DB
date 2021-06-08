1dbe73a62ae3725aa4963e993bf0edcef2081538
Kyanka <mozgoprod@gmail.com>
2021-06-08 09:40:40 +0300

    lab 1-3

---
+++ lab1/helpers/config.py
@@ -0,0 +1,10 @@
+task1_url = "http://uartlib.org/"
+pages_quantity = 20
+task1_output = "task1.xml"
+
+task3_url = "https://www.hozmart.com.ua/uk/recommends/page:"
+goods_quantity = 20
+task3_output = "task3.xml"
+
+task4_xsl = "task4.xsl"
+task4_output = "task4.html"
